a8a049d336d778b9c9b36ac1bd02991403337840
AakulMalhotra <amalhotra_be20@thapar.edu>
2025-12-18 00:14:38 +0530

    new changes added

--- test_login.py
+++ test_login.py
@@ -1,1 +1,24 @@
-print("hello ")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+URL = "https://www.saucedemo.com/"
+VALID_USERNAME = "standard_user"
+VALID_PASSWORD = "secret_sauce"
+INVALID_PASSWORD = "wrong_password"
+
+def setup_driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(URL)
+    return driver
+
+def test_valid_login():
+    driver = setup_driver()
+    wait = WebDriverWait(driver, 10)
+
+
+
+
